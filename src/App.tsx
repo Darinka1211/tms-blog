5c452fb3a51f6dd21e3b0c327f7ee21fe2894b07
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-05-15 22:41:13 +0300

    feat(components): feat(components): добавить кнопки
    добавить кнопки
    добавить onClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,52 @@
-import React from 'react';
-
+import React from "react";
+import { Button } from "./components/Button";
+import BookmarkIcon from "./components/Img/flag.svg";
+import dislikeIcon from "./components/Img/dislike.svg";
+import likeIcon from "./components/Img/like.svg";
+import "../src/App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <Button
+        text="Primary"
+        onClick={() => console.log("Text")}
+        className="button-primary"
+        disable={false}
+      />
+      <Button
+        text="Secondary"
+        onClick={() => console.log("Text")}
+        className="button-secondary"
+        disable={false}
+      />
+      <Button
+        text="Secondary 2"
+        onClick={() => console.log("Text")}
+        className="button-secondary2"
+        disable={false}
+      />
+      <Button
+        text="Button with icon"
+        onClick={() => console.log("Text")}
+        className="Bookmark-Icon"
+        disable={false}
+        icon={BookmarkIcon}
+      />
+      <Button
+        text=""
+        onClick={() => console.log("Text")}
+        className="dislike-Icon"
+        disable={false}
+        icon={dislikeIcon}
+      />
+      <Button
+        text=""
+        onClick={() => console.log("Text")}
+        className="like-Icon"
+        disable={false}
+        icon={likeIcon}
+      />
     </div>
   );
 }
