06f975faf90636af5d2de0a7df7cbfef88ecd46d
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-05-15 23:59:53 +0300

    feat(components): add routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 
 function App() {
+
   return (
     <div className="App">
-     
+      <nav>
+        <Link to="/posts">Posts</Link>
+        <Link to="/posts/1">Post 1</Link>
+        <Link to="/posts/add">Add post</Link>
+        <Link to="/search">Search</Link>
+      </nav>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {AddPost} from "./routers/AddPost/addPost";
+import {Posts} from "./routers/Posts/posts";
+import {Post} from "./routers/Post/post";
+import { Search } from "./routers/Search/search";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+<Routes>
+   <Route path="/" element={<App/>}></Route>
+   <Route path="/posts" element={<Posts/>}></Route>
+   <Route path="/posts/add" element={<AddPost/>}></Route>
+   <Route path="/posts/:id" element={<Post/>}></Route>
+   <Route path="/search" element={<Search/>}></Route>
+   <Route path="*" element={
+     <main style={{padding: "1rem"}}>
+       <p>404 Not Found</p>
+     </main>
+   }></Route>
+
+    </Routes>
+</BrowserRouter>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
