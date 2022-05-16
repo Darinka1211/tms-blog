cf72683bb4e565307d81da09ddcda54cef469d8f
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-05-16 19:48:53 +0300

    feat(components): подключить  store  в index.tsx

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 export const store = configureStore({
-  reducer: {},
+  reducer: {}, /* глобальный объект , котррый хранит инфу  ниже уже указаны типы которые дружат с TS */
 });
  export type RootState = ReturnType<typeof store.getState>
  export type AppDispatch= typeof store.dispatch
