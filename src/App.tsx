1cf290abeb369352f612cb50222472a11845dcd0
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-05-16 19:31:30 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Header } from "./components/Header/Header";
 
 function App() {
-
   return (
     <div className="App">
-      <nav>
-        <Link to="/posts">Posts</Link>
-        <Link to="/posts/1">Post 1</Link>
-        <Link to="/posts/add">Add post</Link>
-        <Link to="/search">Search</Link>
-      </nav>
+      <Header></Header>
+    
+      {/* <nav>
+        
+          <Link to="/posts">Posts</Link>
+          <Link to="/posts/1">Post 1</Link>
+          <Link to="/posts/add">Add post</Link>
+          <Link to="/search">Search</Link>
+        
+      </nav> */}
+      
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+import Burger from "../Img/Burger.svg";
+import Search from "../Img/Search .svg";
+
+type HeaderProps = {
+  className?: string;
+  image?: string;
+  
+};
+
+export const Header = (props: HeaderProps) => {
+  return (
+    <div className="Header__container">
+      <div className="Burger__container">
+        <button className="burger Header__btn">
+          <img src={Burger} alt="burger" />
+        </button>
+        <div className="dropdown-content">
+        <a href="#">Home</a>
+        <a href="#">Add Post</a>
+        </div>
+      </div>
+      <div className="Search__container">
+        <button className="search Header__btn">
+          <img src={Search} alt="search" />
+        </button>
+        <div className="UserName">
+          <span className="UserName__abbr">AM</span>
+          <span className="UserName__name">Artem Malkin</span>
+        </div>
+
+      </div> 
+      
+
+    </div>
+  );
+};
