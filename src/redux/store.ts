7ab9e32ce13dea275bfdcb6ea2f18e24d80d8824
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-05-16 20:05:35 +0300

    feat(components): add  theme: themeReducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from "./redusers/theme";
 export const store = configureStore({
-  reducer: {}, /* глобальный объект , котррый хранит инфу  ниже уже указаны типы которые дружат с TS */
+  reducer: { 
+      theme: themeReducer,
+  }, /* глобальный объект , котррый хранит инфу  ниже уже указаны типы которые дружат с TS */
 });
  export type RootState = ReturnType<typeof store.getState>
  export type AppDispatch= typeof store.dispatch
